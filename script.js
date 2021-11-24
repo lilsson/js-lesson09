f64d01c2a34de26e1676dc103dee6cf04e3f50e7
lilsson <pirckhelavalile@gmail.com>
2021-11-24 19:48:14 +0300

    add css

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
-function getComments(commentsId, callback) {
-    let req = new XMLHttpRequest ();
+// *1
+// function getPost(postId) {
+//     return new Promise(function(resolve, reject){
+//         setTimeout(function() {
+//              if(postId < 100) {
+//               resolve('hello');
+//           }else{
+//               reject('error');
+//           }
+//         }, 1000)
 
-    req.addEventListener('load', callback );
+//     });
+// }
+// function getPostTitle(postTitle) {
+//     return new Promise(function(resolve, reject){
+//         setTimeout(function() {
+//              if(postTitle < 100) {
+//               resolve('hello');
+//           }else{
+//               reject('error');
+//           }
+//         }, 1000)
 
-    req.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-    req.send();
-}
+//     });
+// }
 
-function getPost(postId) {
-    return new Promise(function(resolve, reject){
-        setTimeout(function() {
-             if(postId < 100) {
-              resolve('hello');
-          }else{
-              reject('error');
-          }
-        }, 1000)
+// Promise.all([
+//     getPost(50),
+//     getPostTitle(10)
+// ]).then(function(responses) {
+//     console.log(responses);
+// })
+// .catch(function(error) {
+//     console.log(error);
+// });
+// *2
+// function test1() {
+//     setTimeout( () => {
+//         console.log('test1');
+//     },  3000)
+// }
 
-    });
-}
-function getPostTitle(postTitle) {
-    return new Promise(function(resolve, reject){
-        setTimeout(function() {
-             if(postTitle < 100) {
-              resolve('hello');
-          }else{
-              reject('error');
-          }
-        }, 1000)
+// function test2() {
+//     setTimeout( () => {
+//         console.log('test2');
+//     },  2000)
+// }
 
-    });
-}
+// async function testFunction() {
+//     await test1();
+//     await test2();
+// }
 
+//  testFunction ();
+//  console.log('hello');
 
-getPost(50)
-.then(function(response) {
-    return getPostTitle(10);
-})
+// *3
 
-.then(function(postData) {
+// function getPost(postId) {
+//         return new Promise(function(resolve, reject){
+//             setTimeout(function() {
+//                  if(postId < 100) {
+//                   resolve('hello');
+//               }else{
+//                   reject('error');
+//               }
+//             }, 1000)
     
-})
-.catch(function(error) {
-    console.log(error);
-});
+//         });
+//     }
+//     function getPostTitle(postTitle) {
+//         return new Promise(function(resolve, reject){
+//             setTimeout(function() {
+//                  if(postTitle < 100) {
+//                   resolve('hello');
+//               }else{
+//                   reject('error2');
+//               }
+//             }, 1000)
+    
+//         });
+//     }
+    
+//     async function renderPage() {
+//         let posts;
+        
+//         try {
+//            posts = await getPost(10);
+//         }
+        
+//         catch(error) {
+//             console.log(error);
+//         }
+        
+//     }
+
+//     renderPage();
